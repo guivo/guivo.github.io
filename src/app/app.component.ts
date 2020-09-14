23dbb0e76131ba33198b48ba63e6ff2215c6d1a2
Guido Volpi <guido.volpi@neat.it>
2020-09-14 18:26:54 +0200

    Introduction of angular
    Basic toolbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-page';
+
+  //constructor(iconRegistry: MatIconRegistry) {
+  //  iconRegistry.registerFontClassAlias();
+  //}
 }
